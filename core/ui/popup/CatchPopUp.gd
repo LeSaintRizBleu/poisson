ef802592d5a74e390a065542daf6aec6e3e500e7
RizBleu <mateo.galizzi05@gmail.com>
2025-11-28 18:58:32 +0100

    better centering of the fish inside the catch popup

--- core/ui/popup/CatchPopUp.gd
+++ core/ui/popup/CatchPopUp.gd
@@ -10,8 +10,15 @@ var fish_info: FishInfo
 
 func _ready() -> void:
 	description.text = "It's a " + fish_info.get_fish_name() + " !\n\n" + fish_info.get_description()
-	fish.texture = fish_info.get_sprite()
-	var new_offset: Vector2 = Vector2(fish_info.get_sprite().get_height()/2.0, fish_info.get_sprite().get_height()/2.0)
+	apply_texture()
+
+func apply_texture() -> void:
+	var texture: CompressedTexture2D = fish_info.get_sprite()
+	var new_size: Vector2 = texture.get_size()
+	var new_offset: Vector2 = texture.get_size()/2.0
+	fish.texture = texture
+	fish.size = new_size
+	fish.set_anchors_preset(Control.PRESET_CENTER)
 	fish.pivot_offset = new_offset
 
 func _on_button_pressed() -> void:
