00fe58805bd78320032f514c859e6182d7645262
Riz Bleu <mateo.galizzi05@gmail.com>
2026-06-04 00:03:58 +0200

    fix fish card spacing

--- core/ui/inventory/FishCard.gd
+++ core/ui/inventory/FishCard.gd
@@ -5,16 +5,17 @@ var fish_type: String
 
 signal load_description
 
-@onready var label: Label = $MarginContainer/MarginContainer/Name
-@onready var sprite: TextureRect = $MarginContainer/MarginContainer/Control/Sprite
+@onready var label: Label = $MarginContainer/MarginContainer/LabelControl/Name
+@onready var sprite: TextureRect = $MarginContainer/MarginContainer/SpriteControl/Sprite
 
 func _ready() -> void:
 	load_card()
 
 func load_card() -> void:
-	label.text = fish_type
-	var url: String = "res://assets/fishes/" + fish_type + ".png"
-	sprite.texture = load(url)
+	var url: String = "res://resources/fishesInfo/" + fish_type + ".tres"
+	var info: FishInfo = load(url)
+	label.text = info.name
+	sprite.texture = info.sprite
 
 func _on_margin_container_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
